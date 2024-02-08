ffc501e74aaec849bb370fcfa262f5aaffbfc85c
Aditya Ibrar Abdillah <adityaibrar@gmail.com>
2024-02-09 00:07:01 +0700

    redesign ui navbar and theme

--- layout/content.php
+++ layout/content.php
@@ -1,13 +1,16 @@
 <?php
 $NUM = $admin->runQuery("SELECT COUNT(id_user) AS jumlah_vote FROM hasil_voting");
 $NUM->execute();
-$NUMS=$NUM->fetch(PDO::FETCH_ASSOC); $masuk = $NUMS['jumlah_vote'];
+$NUMS = $NUM->fetch(PDO::FETCH_ASSOC);
+$masuk = $NUMS['jumlah_vote'];
 
 $stmt = $admin->runQuery("SELECT * FROM calon_osis");
 $stmt->execute();
 
 $query = $admin->runQuery("SELECT COUNT(nama) as jumlah_user FROM users WHERE akses!=1");
-$query->execute(); $user = $query->fetch(); $total = $user['jumlah_user'];
+$query->execute();
+$user = $query->fetch();
+$total = $user['jumlah_user'];
 ?>
 
 <!--main content start-->
@@ -18,14 +21,14 @@ $query->execute(); $user = $query->fetch(); $total = $user['jumlah_user'];
 			<div class="col-xs-6 market-update-gd">
 				<div class="market-update-block clr-block-3">
 					<div class="col-xs-4 market-update-right">
-						<i class="fa fa-users" ></i>
+						<i class="fa fa-users"></i>
 					</div>
 					<div class="col-xs-8 market-update-left">
-					<h4>Sudah Memilih</h4>
+						<h4>Sudah Memilih</h4>
 						<h3><?= $masuk ?></h3>
 						<p>orang</p>
 					</div>
-				  <div class="clearfix"> </div>
+					<div class="clearfix"> </div>
 				</div>
 			</div>
 			<div class="col-xs-6 market-update-gd">
@@ -35,47 +38,54 @@ $query->execute(); $user = $query->fetch(); $total = $user['jumlah_user'];
 					</div>
 					<div class="col-xs-8 market-update-left">
 						<h4>Belum memilih</h4>
-						<h3><?= $total-$masuk?></h3>
+						<h3><?= $total - $masuk ?></h3>
 						<p>orang</p>
 					</div>
-				  <div class="clearfix"> </div>
+					<div class="clearfix"> </div>
 				</div>
 			</div>
-		   <div class="clearfix"> </div>
+			<div class="clearfix"> </div>
 		</div>
 		<!-- //market-->
 		<div class="row">
 			<div class="panel-body">
 				<div class="col-md-12">
 					<!--agileinfo-grap-->
-						<div class="agileinfo-grap">
-							<div class="agileits-box">
-								<header class="agileits-box-header clearfix">
-									<h3>
-										E-VOTE KANETA
+					<div class="agileinfo-grap">
+						<div class="agileits-box">
+							<header class="agileits-box-header clearfix">
+								<h3>
+									E-VOTE KANETA
 
-									</h3>
+								</h3>
 
-								</header>
-								<div class="agileits-box-body clearfix">
-									<br>
-									<p align="center">
-										<img src="../../assets/logo/kaneta
+							</header>
+							<div class="agileits-box-body clearfix">
+								<br>
+								<p align="center">
+									<img src="../../assets/logo/kaneta
 										.png" width="20%" alt="SMK Negeri 1 Tapen">
-									</p>
-									<br><br>
-									<p align="center">
-										E-VOTE adalah sebuah Sistem Informasi yang bertujuan untuk memfasilitasi Kegiatan Sekolah
-										 untuk mencari calon Ketua OSIS di SMK Negeri 1 Tapen Tahun Periode 2022/2023.
-									</p>
-									<style type="text/css">;
-									    h3 {font-family:  Cambria,"Times New Roman",serif}
-									    p { font-family:  Cambria,"Times New Roman",serif }
-									</style>
-								</div>
+								</p>
+								<br><br>
+								<p align="center">
+									E-VOTE adalah sebuah Sistem Informasi yang bertujuan untuk memfasilitasi Kegiatan Sekolah
+									untuk mencari calon Ketua OSIS di SMPN1 Maesan Bondowoso Tahun Periode 2024/2025.
+								</p>
+								<style type="text/css">
+									;
+
+									h3 {
+										font-family: 'Poppins', sans-serif
+									}
+
+									p {
+										font-family: 'Poppins', sans-serif
+									}
+								</style>
 							</div>
 						</div>
-	<!--//agileinfo-grap-->
+					</div>
+					<!--//agileinfo-grap-->
 
 				</div>
 			</div>
--- page/polling/index.php
+++ page/polling/index.php
@@ -1,5 +1,5 @@
 <?php
-$page='polling';
+$page = 'polling';
 
 include_once '../../layout/cek_id.php';
 include_once '../../layout/header.php';
@@ -8,22 +8,22 @@ include_once '../../layout/navigation.php';
 <section id="main-content">
 	<section class=" wrapper">
 		<div class="agile-grid">
-		<h2 style="text-align:center;margin-bottom:20px;">HASIL QUICK COUNT PEMILIHAN KETUA OSIS</h2>
-    		<div class="row" id="load_data">
-			
+			<h2 style="text-align:center;margin-bottom:20px; font-family: 'Poppins', sans-serif">HASIL QUICK COUNT PEMILIHAN KETUA OSIS</h2>
+			<div class="row" id="load_data">
+
 			</div>
 		</div>
-</section>
-<?php
+	</section>
+	<?php
 	include_once '../../layout/footer.php';
-?>
+	?>
 
-<script>
-$(document).ready(function(){
- setInterval(function(){//setInterval() method execute on every interval until called clearInterval()
-  $('#load_data').load("../polling/fetch.php").fadeIn("slow");
-  //load() method fetch data from fetch.php page
- }, 1000);
- 
-});
-</script>
+	<script>
+		$(document).ready(function() {
+			setInterval(function() { //setInterval() method execute on every interval until called clearInterval()
+				$('#load_data').load("../polling/fetch.php").fadeIn("slow");
+				//load() method fetch data from fetch.php page
+			}, 1000);
+
+		});
+	</script>
